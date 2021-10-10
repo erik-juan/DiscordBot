1c3b8cd52c1eaaed91bb7ee2d98e9cd41ce19779
Erik Poole <erikjpoole@gmail.com>
2021-10-09 18:45:39 -0600

    using absolute path

--- scripts/start-bot.sh
+++ scripts/start-bot.sh
@@ -1,4 +1,4 @@
-cd ..
+cd ~/discord_bot
 docker rmi -f mahkumazahn/discord_bot
 docker build -t mahkumazahn/discord_bot .
 docker run --rm --env-file .env -v ~/logs/discord_bot:/logs mahkumazahn/discord_bot
