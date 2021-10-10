d3149ad56a7eef4d22c7f3eec3ecc2bcefb75fc9
Erik Poole <erikjpoole@gmail.com>
2021-10-09 22:54:25 -0600

    stopping container before removing image

--- scripts/start-bot.sh
+++ scripts/start-bot.sh
@@ -1,4 +1,5 @@
 cd ~/discord_bot
+docker stop discord_bot
 docker rmi -f mahkumazahn/discord_bot
 docker build -t mahkumazahn/discord_bot .
-docker run -d --rm --env-file .env -v ~/logs/discord_bot:/logs mahkumazahn/discord_bot
+docker run -d --rm --env-file .env -v ~/logs/discord_bot:/logs --name discord_bot mahkumazahn/discord_bot
